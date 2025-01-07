60ff8054d823fff1e93ee56f55992b8927181560
ry129 <0111cs211146.riya@gmail.com>
2025-01-07 14:40:53 +0530

    push updated code

--- calculator.java
+++ calculator.java
@@ -7,6 +7,10 @@ return a+b;
 public static int sub(int a , int b){
 return a-b;
 }
+public static int mul(int a , int b){
+return a*b;
+}
+
 
 public static void main(String [] args) {
 System.out.println("Welcome to Calculator");
@@ -16,8 +20,5 @@ sub(5,5);
 mul(10,10);
 } 
 
-public static int mul(int a , int b){
-return a*b;
-}
 
 }
